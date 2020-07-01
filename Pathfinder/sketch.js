c71a14ea69c77a780209218a5c55ed99421d2393
Deepak Tiwari <deepakait5090@gmail.com>
2020-07-02 02:47:34 +0530

    Updated script.js:
    1.Improved Speed of Execution
    2.Used Priority_queue in-place of  array
    3.Resolved some bugs
    4.Enhanced UI

--- Pathfinder/sketch.js
+++ Pathfinder/sketch.js
@@ -3,16 +3,12 @@
 //1.Program starts from setup() function.Go there first.Line nm:72
 //Refer Wikipedia A* algo page for learning about algorithm
 
-var rows = 25;
-var cols = 52;
+var rows = 21;
+var cols = 45;
 var grid = new Array(cols);
 var check = false;
-
-//Stores cells which need to be visited
-var openset = [];
-//Stores cells which are finished visiting!
+var pqueue;
 var closedset = [];
-
 var start;
 var end;
 var h, w;
@@ -20,17 +16,18 @@ var path = [];
 
 //Property function of each Cell
 function Spot(i, j) {
-
     this.i = i;
     this.j = j;
-    this.f = 0;
-    this.h = 0;
-    this.g = 0;
+    this.f = Infinity;
+    this.h = Infinity;
+    this.g = Infinity;
+    this.visited = false;
     this.neighbours = [];
     this.camefrom = null;
     this.wall = false;
 
-    if (random(1) < 0.2) {
+    //random wall creation
+    if (random(1) < 0.4) {
         this.wall = true;
     }
 
@@ -76,30 +73,54 @@ function Spot(i, j) {
             this.neighbours.push(grid[i + 1][j - 1]);
         }
         */
-
     }
 }
 
-//This function removes specified cell from openset,as it has been visited
+//priority queue element
+class queue_element {
+    constructor(i, j) {
 
-function removefromarray(arr, ele) {
+        this.element = grid[i][j];
+        this.priority = grid[i][j].f;
+    }
+}
 
-    for (var i = arr.length - 1; i >= 0; i--) {
-        if (arr[i] == ele) {
-            arr.splice(i, 1);
-            return;
+//priority queue definitoins and method 
+class priority_queue {
+
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue = function(i, j) {
+
+        var ele = new queue_element(i, j);
+        var contain = false;
+
+        for (var i = 0; i < this.items.length; i++) {
+            if (this.items[i].priority > ele.priority) {
+                this.items.splice(i, 0, ele);
+                contain = true;
+                break;
+            }
+        }
+        if (!contain) {
+            this.items.push(ele);
         }
     }
-}
 
-//making educated guess of euclidean distance
+    dequeue = function() {
 
-function heuristic(a, b) {
-    //euclidean
-    var eud = Math.sqrt((a.i - b.i) * (a.i - b.i) + (a.j - b.j) * (a.j - b.j));
-    //manhattan
-    var man = abs(a.i - b.i) + abs(a.j - b.j);
-    return man;
+        this.items.splice(0, 1);
+    }
+
+    front = function() {
+        return this.items[0];
+    }
+
+    isEmpty = function() {
+        return this.items.length == 0;
+    }
 }
 
 //Main function.starts from here!
@@ -107,16 +128,12 @@ function setup() {
 
     createCanvas(1360, 650);
     console.log("Pathfinder");
-
     h = height / rows;
     w = width / cols;
 
-
     //making a 2D array.An 1D array of grid created before.See at line 8
-
     for (var i = 0; i < cols; i++)
         grid[i] = new Array(rows);
-
 
     //Assigning property to each cell of 2D grid i.e. its f,g,h value 
     for (var i = 0; i < cols; i++) {
@@ -124,12 +141,12 @@ function setup() {
             grid[i][j] = new Spot(i, j); //refer to line 20.Spot function is just like a constructor function initialising property of each cell to zero.
         }
     }
+
     //start from top left corner
-    start = grid[0][0];
+    start = grid[5][10];
     //end at bottom right corner
-    end = grid[cols - 1][rows - 1];
+    end = grid[35][10];
     //end = grid[29][5];
-    //start with the initial position.
 
     start.wall = false;
     end.wall = false;
@@ -140,7 +157,18 @@ function setup() {
             grid[i][j].addneighbours(grid); //Refer to line 37
         }
     }
-    openset.push(start);
+
+    //Creating a priority_queue instance
+    pqueue = new priority_queue();
+    start.g = 0;
+    start.f = 0;
+    pqueue.enqueue(start.i, start.j);
+
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].showyou(color(255));
+        }
+    }
 }
 
 //this function repeats itself again and again
@@ -148,23 +176,15 @@ function draw() {
 
     //A* starts from here
     //there are some cells remainging to be visited
-    if (openset.length > 0) {
+    if (!pqueue.isEmpty()) {
 
-        //calcuting the cell which needs to be visited with the minimum 'f' value
-        var winner = 0;
+        var current = pqueue.front().element;
 
-        for (var i = 0; i < openset.length; i++) {
-            if (openset[i].f < openset[winner].f) {
-                winner = i;
-            }
-        }
-
-        var current = openset[winner];
         //if that unvisited cell is our destination
         if (current === end) {
             check = true;
             console.log("DONE!");
-            //for stopping the calling of draw() function again.
+
             path = [];
             var temp = current;
             path.push(current);
@@ -176,45 +196,42 @@ function draw() {
 
             noFill();
             stroke(255, 245, 102);
-            strokeWeight(w / 8);
+            strokeWeight(w / 7);
             beginShape();
             for (var i = 0; i < path.length; i++) {
                 vertex(path[i].i * w + w / 2, path[i].j * h + h / 2);
             }
             endShape();
 
+            //for stopping the calling of draw() function again.
             noLoop();
         } else {
             //remove that current cell from openset as it has been visited
-            removefromarray(openset, current);
-
+            pqueue.dequeue();
             //and aad it to the closed set
             closedset.push(current);
-
             //taking all the neighbors of current cell
             var neigh = current.neighbours;
 
             for (var i = 0; i < neigh.length; i++) {
                 var neighbor = neigh[i];
                 //if that neighbour is not in closed set,then we only need to visit
-                if (!closedset.includes(neighbor)) {
+                if (!neighbor.visited) {
+                    neighbor.visited = true;
                     var tempG = current.g + 1;
 
-                    // Updating minimum distance of any node if it has been visited before in the same loop
-                    if (openset.includes(neighbor)) {
-                        if (tempG < neighbor.g) {
-                            neighbor.g = tempG;
-                            neighbor.h = heuristic(neighbor, end);
-                            neighbor.f = neighbor.g + neighbor.h;
-                            neighbor.camefrom = current;
-                        }
-                    } else {
+                    if (tempG < neighbor.g) {
                         neighbor.g = tempG;
-                        openset.push(neighbor);
-                        neighbor.h = heuristic(neighbor, end);
-                        neighbor.f = neighbor.g + neighbor.h;
                         neighbor.camefrom = current;
 
+                    }
+                    var temph = Math.abs(neighbor.i - end.i) + Math.abs(neighbor.j - end.j); //Math.sqrt((neighbor.i - end.i) * (neighbor.i - end.i) + (neighbor.j - end.j) * (neighbor.j - end.j)); //Math.abs(neighbor.i - end.i) + Math.abs(neighbor.j - end.j);
+                    var newcost = neighbor.g + temph;
+
+                    if (newcost < neighbor.f) {
+                        neighbor.h = temph;
+                        neighbor.f = newcost;
+                        pqueue.enqueue(neighbor.i, neighbor.j);
                     }
                 }
             }
@@ -222,67 +239,27 @@ function draw() {
         }
 
         if (!check) {
-            background(0);
-            for (var i = 0; i < cols; i++) {
-                for (var j = 0; j < rows; j++) {
-                    grid[i][j].showyou(color(255));
-                }
-            }
 
-
-            for (var i = 0; i < openset.length; i++) {
+            for (var i = 0; i < pqueue.items.length; i++) {
                 //if in neighbour of current node,colour them green
-                openset[i].showyou(color(177, 250, 82));
+                pqueue.items[i].element.showyou(color(177, 250, 82));
             }
 
             for (var i = 0; i < closedset.length; i++) {
-                //if already visited colour them red
+                //if already visited colour them blue
                 closedset[i].showyou(color(74, 247, 244));
             }
 
-            //If want to show path on runtime :)
-            /*
-            path = [];
-            var temp = current;
-            path.push(current);
-            while (temp.camefrom) {
-                path.push(temp.camefrom);
-                temp = temp.camefrom;
-            }
-
-            for (var i = 0; i < path.length; i++) {
-                path[i].showyou(color(255, 231, 6));
-            }
-
-            //Drawing the line in the path
-
-            noFill();
-            stroke(255, 0, 255);
-            strokeWeight(w / 4);
-            beginShape();
-            for (var i = 0; i < path.length; i++) {
-                vertex(path[i].i * w + w / 2, path[i].j * h + h / 2);
-            }
-            endShape();
-
-            */
             start.showyou(color(0, 255, 0));
             end.showyou(color(255, 0, 0));
         }
     } else {
 
         console.log("No path Exist!");
-        noLoop();
-
-        for (var i = 0; i < cols; i++) {
-            for (var j = 0; j < rows; j++) {
-                //white color for all grids
-                grid[i][j].showyou(color(255));
-            }
-        }
         start.showyou(color(0, 255, 0));
         end.showyou(color(255, 0, 0));
+        //for stopping the calling of draw() function again.
+        noLoop();
     }
 }
-
 //end of the code!!
